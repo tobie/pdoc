c9b1759ae36341b3126775437aab3210498e7544
Andrew Dupont <prototype@andrewdupont.net>
2008-05-01 11:16:45 -0500

    Added keyboard navigability to the search menu.

--- templates/html/assets/javascripts/application.js
+++ templates/html/assets/javascripts/application.js
@@ -45,13 +45,20 @@ var Filterer = Class.create({
       resultsElement: '.search-results'
     }, options || {});
     
+    this.element.writeAttribute("autocomplete", "off");    
+    this.element.up('form').observe("submit", function(event) { event.stop(); });
+    
+    if (Prototype.Browser.WebKit)
+      this.element.type = "search";
+    
     this.menu = this.options.menu;
     this.links = this.menu.select('a');
     
     this.resultsElement = this.options.resultsElement;
     
     this.events = {
-      filter:   this.filter.bind(this)
+      filter:   this.filter.bind(this),
+      keypress: this.keypress.bind(this)
     };
     
     this.menu.setStyle({ opacity: 0.9 });
@@ -65,12 +72,15 @@ var Filterer = Class.create({
   },
   
   filter: function(event) {
+    // clear the text box on ESC
     if (event.keyCode && event.keyCode === Event.KEY_ESC) {
       this.element.value = '';
     }
     
-    var value = $F(this.element).strip().toLowerCase();
+    if ([Event.KEY_UP, Event.KEY_DOWN, Event.KEY_RETURN].include(event.keyCode))
+      return;
     
+    var value = $F(this.element).strip().toLowerCase();    
     if (value === "") {
       this.onEmpty();
       return;
@@ -78,6 +88,44 @@ var Filterer = Class.create({
     
     var urls  = this.findURLs(value);  
     this.buildResults(urls);
+  },
+  
+  keypress: function(event) {
+    if (![Event.KEY_UP, Event.KEY_DOWN, Event.KEY_RETURN].include(event.keyCode))
+      return;
+      
+    event.stop();
+    
+    var highlighted = this.resultsElement.down('.highlighted');
+    if (event.keyCode === Event.KEY_RETURN) {
+      // follow the highlighted item
+      if (!highlighted) return;
+      window.location.href = highlighted.down('a').href;
+    } else {
+      // move the focus
+      if (!highlighted) {
+        var highlighted = this.resultsElement.down('li').addClassName('highlighted');
+      } else {
+        var method = (Event.KEY_DOWN === event.keyCode) ? 'next' : 'previous';
+        highlighted.removeClassName('highlighted');
+        var adjacent = highlighted[method]('li');
+        if (!adjacent) {
+          adjacent = method == 'next' ? this.resultsElement.down('li') :
+           this.resultsElement.down('li:last-of-type');
+        }
+        adjacent.addClassName('highlighted');
+        highlighted = adjacent;
+      }
+    }
+    
+    // Adjust the scroll offset of the container so that the highlighted
+    // item is always in view.
+    var distanceToBottom = highlighted.offsetTop + highlighted.offsetHeight;
+    if (distanceToBottom > this.resultsElement.offsetHeight + this.resultsElement.scrollTop) {
+      this.resultsElement.scrollTop = distanceToBottom - this.resultsElement.offsetHeight;
+    } else if (highlighted.offsetTop < this.resultsElement.scrollTop) {
+      this.resultsElement.scrollTop = highlighted.offsetTop;
+    }
   },
   
   buildResults: function(urls) {
@@ -92,8 +140,9 @@ var Filterer = Class.create({
       li.appendChild(a);
       ul.appendChild(li);
     });    
-    this.resultsElement.show();
+    this.showResults();
   },
+    
   
   findURLs: function(str) {
     var results = [];
@@ -104,8 +153,18 @@ var Filterer = Class.create({
   },
   
   onEmpty: function() {
+    this.hideResults();
+  },
+  
+  showResults: function() {
+    this.resultsElement.show();
+    document.observe("keydown", this.events.keypress);
+  },
+  
+  hideResults: function() {
     this.resultsElement.hide();
-  }
+    document.stopObserving("keydown", this.events.keypress);
+  }  
 });
 
 document.observe('dom:loaded', function() {
