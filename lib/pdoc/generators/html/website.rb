tobie/pdoc

commit 9f366ec1956e3a3dc4b7caf559f7d639a208eec8

Fix issue that prevented rendering of utility methods.

lib/pdoc/generators/html/website.rb
@@ -30,7 +30,9 @@ module PDoc
             render_template('section', "#{section.id}.html", {:doc_instance => section})
           end
           
-          root.namespaces.each(&method(:render_namespace))
+          root.utilities.each(&method(:render_namespace))
+          
+          root.namespaces.each(&method(:render_utility))
           
           copy_assets
           
@@ -52,6 +54,13 @@ module PDoc
         def render_namespace(object)
           template, path = find_template_name(object), path(object)
           @depth = path.size
+          dest = File.join(path, "#{object.id}.html")
+          render_template(template, dest, {:doc_instance => object})
+        end
+
+        def render_utility(object)
+          template, path = find_template_name(object), path(object)
+          @depth = path.size - 1
           dest = File.join(path, "#{object.id}.html")
           render_template(template, dest, {:doc_instance => object})
         end

commit 2c333a416f2895acc10140b6928d8ddeac19a674

Fix multiple bugs caused by a typo introduced in 9f366ec1956e3a3dc4b7caf559f7d639a208eec8.
Signed-off-by: Tobie Langel <tobie.langel@gmail.com>

lib/pdoc/generators/html/website.rb
@@ -30,9 +30,8 @@ module PDoc
             render_template('section', "#{section.id}.html", {:doc_instance => section})
           end
           
-          root.utilities.each(&method(:render_namespace))
-          
-          root.namespaces.each(&method(:render_utility))
+          root.utilities.each(&method(:render_utility))
+          root.namespaces.each(&method(:render_namespace))
           
           copy_assets
           