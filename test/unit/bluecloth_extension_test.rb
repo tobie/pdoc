6592d026be188d77a8e42765dd88f72d61ff3f43
Tobie Langel <tobie.langel@gmail.com>
2009-08-12 03:02:49 +0200

    Rewrote failing BlueCloth tests the right way round (easier to figure out what's going on).

--- test/unit/bluecloth_extension_test.rb
+++ test/unit/bluecloth_extension_test.rb
@@ -3,13 +3,10 @@ require 'test/unit'
 
 class BlueClothExtensions < Test::Unit::TestCase
   def test_code_block
-    assert_equal BlueCloth.new("\n\n    foo\n").to_html,
-      "<pre><code class=\"javascript\">foo\n</code></pre>"
+    assert_equal "<pre><code class=\"javascript\">foo\n</code></pre>", BlueCloth.new("\n\n    foo\n").to_html
 
-    assert_equal BlueCloth.new("\n\n    lang: html\n    foo\n").to_html,
-      "<pre><code class=\"html\">foo\n</code></pre>"
+    assert_equal "<pre><code class=\"html\">foo\n</code></pre>", BlueCloth.new("\n\n    lang: html\n    foo\n").to_html
       
-    assert_equal BlueCloth.new("\n\n    language: css\n    foo\n").to_html,
-      "<pre><code class=\"css\">foo\n</code></pre>"
+    assert_equal "<pre><code class=\"css\">foo\n</code></pre>", BlueCloth.new("\n\n    language: css\n    foo\n").to_html
   end
 end
