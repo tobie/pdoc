914a9ea769e1c211184bd2a15d38097d11046bd2
Tobie Langel <tobie.langel@gmail.com>
2009-08-25 20:47:14 +0200

    Update defautl templates.

--- templates/html/assets/javascripts/application.js
+++ templates/html/assets/javascripts/application.js
@@ -1,10 +1,30 @@
 if (typeof PDoc === "undefined") window.PDoc = {};
 
-PDoc.highlightSelected = function(element) {
-  if (!element && !window.location.hash) return;
-  element = (element || $(window.location.hash.substr(1)));
+// Poor-man's history manager. Polls for changes to the hash.
+(function() {  
+  var PREVIOUS_HASH = null;
+  
+  Event.observe(window, "load", function() {    
+    var hash = window.location.hash;
+    if (hash && hash !== PREVIOUS_HASH) {
+      document.fire("hash:changed",
+       { previous: PREVIOUS_HASH, current: hash });        
+      PREVIOUS_HASH = hash;      
+    }
+    
+    window.setTimeout(arguments.callee, 100);  
+  });  
+})();
+
+// Place a "frame" around the element described by the hash.
+// Update the frame when the hash changes.
+PDoc.highlightSelected = function() {
+  if (!window.location.hash) return;  
+  element = $(window.location.hash.substr(1));
   if (element) PDoc.highlight(element.up('li, div'));
 };
+
+document.observe("hash:changed", PDoc.highlightSelected);
 
 PDoc.highlight = function(element) {
   var self = arguments.callee;
@@ -17,26 +37,29 @@ PDoc.highlight = function(element) {
   
   element.getOffsetParent().appendChild(frame);
   
-  frame.clonePosition(element, { offsetLeft: -5, offsetTop: -5 });
+  var offset = element.positionedOffset();
   var w = parseFloat(element.getStyle('width')),
       h = parseFloat(element.getStyle('height'));
-  
+      
   frame.setStyle({
-    width:  (w + 10) + 'px',
-    height: (h + 10) + 'px'
+    position: 'absolute',
+    top: (offset.top - 15) + 'px',
+    left: (offset.left - 12) + 'px',
+    width:  (w + 20) + 'px',
+    height: (h + 30) + 'px'
   });
+  
+  // Defer this call because Safari hasn't yet scrolled the viewport.
+  (function() {
+    var frameOffset = frame.viewportOffset(frame);
+    if (frameOffset.top < 0) {
+      window.scrollBy(0, frameOffset.top - 10);
+    }    
+  }).defer();
+  
 };
 
-PDoc.HighlightOptions = {
-  startcolor: '#e4e4e4',
-  restorecolor: true,
-  queue: {
-    position:'end',
-    scope: 'global',
-    limit: 1
-  }
-};
-
+// Live API search.
 var Filterer = Class.create({
   initialize: function(element, options) {
     this.element = $(element);
@@ -48,9 +71,9 @@ var Filterer = Class.create({
     this.element.writeAttribute("autocomplete", "off");    
     this.element.up('form').observe("submit", Event.stop);
     
-    // The Safari-only "search" input type is prettier
-    if (Prototype.Browser.WebKit)
-      this.element.type = "search";
+    // // The Safari-only "search" input type is prettier
+    // if (Prototype.Browser.WebKit)
+    //   this.element.type = "search";
     
     this.menu = this.options.menu;
     this.links = this.menu.select('a');
@@ -78,7 +101,7 @@ var Filterer = Class.create({
   filter: function(event) {
     if (this._timer) window.clearTimeout(this._timer);
     
-    // clear the text box on ESC
+    // Clear the text box on ESC
     if (event.keyCode && event.keyCode === Event.KEY_ESC) {
       this.element.value = '';
     }
@@ -96,7 +119,7 @@ var Filterer = Class.create({
     this.buildResults(urls);
   },
   
-  keydown: function(event) {    
+  keydown: function(event) {
     if (![Event.KEY_UP, Event.KEY_DOWN, Event.KEY_RETURN].include(event.keyCode))
       return;
       
@@ -108,7 +131,7 @@ var Filterer = Class.create({
     
     var highlighted = this.resultsElement.down('.highlighted');
     if (event.keyCode === Event.KEY_RETURN) {
-      // follow the highlighted item
+      // Follow the highlighted item.
       if (!highlighted) return;
       window.location.href = highlighted.down('a').href;
     } else {
@@ -119,10 +142,10 @@ var Filterer = Class.create({
     
     if ([Event.KEY_UP, Event.KEY_DOWN].include(event.keyCode) &&
      !Prototype.Browser.WebKit) {    
-      // if up/down key is held down, list should keep scrolling
+      // If up/down key is held down, list should keep scrolling.
       // Safari does this automatically because it fires the keydown
-      // event over and over
-      this._timer = window.setTimeout(this.scrollList.bind(this, direction), 100);
+      // event over and over.
+      this._timer = window.setTimeout(this.scrollList.bind(this, direction), 1000);
     }
   },
   
@@ -163,18 +186,24 @@ var Filterer = Class.create({
     this._timer = window.setTimeout(this.scrollList.bind(this, direction), 100);
   },
   
+  // Given a path with any number of `../`s in front of it, remove them all.
+  // TODO: Fix this a better way.
+  _fixPath: function(path) {
+    return path.replace('../', '');
+  },
+  
   buildResults: function(urls) {
     this.resultsElement.update();
     var ul = this.resultsElement;
     urls.each( function(url) {
       var a  = new Element('a', {
         'class': url.type.gsub(/\s/, '_'),
-        href:    PDoc.pathPrefix + url.path
+        href:    PDoc.pathPrefix + this._fixPath(url.path)
       }).update(url.name);
-      var li = new Element('li');
+      var li = new Element('li', { 'class': 'menu-item' });
       li.appendChild(a);
       ul.appendChild(li);
-    });    
+    }, this);    
     this.showResults();
   },
     
@@ -193,6 +222,7 @@ var Filterer = Class.create({
   
   showResults: function() {
     this.resultsElement.show();
+    document.stopObserving("keydown", this.events.keydown);
     document.observe("keydown", this.events.keydown);
   },
   
@@ -203,20 +233,12 @@ var Filterer = Class.create({
 });
 
 document.observe('dom:loaded', function() {
-  new Filterer($('search'), { menu: $('api_menu'), resultsElement: $('search_results') });
+  new Filterer($('search'), {
+    menu: $('api_menu'), 
+    resultsElement: $('search_results')
+  });
 });
 
-document.observe('click', function(event) {
-  var element = event.findElement('a');
-  if (!element) return;
-  var href = element.readAttribute('href');
-  if (!href.include('#')) return;
-  if (element = $(href.split('#').last())) {
-    PDoc.highlightSelected(element);
-  }
-});
-
-document.observe('dom:loaded', function() { PDoc.highlightSelected() });
 
 Event.observe(window, 'load', function() {
   var menu = $('menu');
@@ -227,5 +249,141 @@ Event.observe(window, 'load', function() {
     if (sOffset.top > OFFSET) {
       menu.addClassName('fixed');
     } else menu.removeClassName('fixed');
-  })
+  });
+});
+
+(function() {
+  function menuButtonMouseOver(event) {
+    var menuButton = $('api_menu_button');
+    var target = event.element();
+    if (target === menuButton || target.descendantOf(menuButton)) {
+      $('api_menu').show();
+    }
+  }
+  
+  function menuButtonMouseOut(event) {
+    var menuButton = $('api_menu_button');
+    var menu = $('api_menu');
+    var target = event.element(), related = event.relatedTarget || event.toElement;
+    
+    if (related && (related === menu || related.descendantOf(menu))) return;
+    menu.hide();
+  }
+  
+  function menuMouseOut(event) {
+    var menu = $('api_menu'), related = event.relatedTarget || event.toElement;
+    if (related && !related.descendantOf(menu)) {
+      arguments.callee.timer = Element.hide.delay(0.5, menu);
+    } else {
+      window.clearTimeout(arguments.callee.timer);
+    }
+  }
+  
+  function menuItemMouseOver(event) {
+    var element = event.element();    
+    if (element.tagName.toLowerCase() === 'a') {
+      element.addClassName('highlighted');
+    }
+  }
+  
+  function menuItemMouseOut(event) {
+    var element = event.element();    
+    if (element.tagName.toLowerCase() === 'a') {
+      element.removeClassName('highlighted');
+    }
+  }
+  
+  var MENU_ITEMS;
+  
+  document.observe('dom:loaded', function() {
+    MENU_ITEMS = $$('.api-box .menu-item a');
+    
+    $('api_menu_button').observe('mouseenter', menuButtonMouseOver);
+    $('api_menu_button').observe('mouseleave', menuButtonMouseOut );
+    
+    $('api_menu').observe('mouseleave', menuMouseOut);
+    
+    if (Prototype.Browser.IE) {
+      $('api_menu').observe('mouseover', menuItemMouseOver);
+      $('api_menu').observe('mouseout',  menuItemMouseOut);
+    }
+  });
+})();
+
+Form.GhostedField = Class.create({
+  initialize: function(element, title, options) {
+    this.element = $(element);
+    this.title = title;
+    
+    options = options || {};
+    
+    this.isGhosted = true;
+    
+    if (options.cloak) {
+      // Wrap the native getValue function so that it never returns the
+      // ghosted value. This is optional because it presumes the ghosted
+      // value isn't valid input for the field.
+      this.element.getValue = this.element.getValue.wrap(this.wrappedGetValue.bind(this));      
+    }    
+    
+    this.addObservers();
+    this.onBlur();
+  },
+  
+  wrappedGetValue: function($proceed) {
+    var value = $proceed();
+    return value === this.title ? "" : value;
+  },
+  
+  addObservers: function() {
+    this.element.observe('focus', this.onFocus.bind(this));
+    this.element.observe('blur',  this.onBlur.bind(this));
+    
+    var form = this.element.up('form');
+    if (form) {
+      form.observe('submit', this.onSubmit.bind(this));
+    }
+    
+    // Firefox's bfcache means that form fields need to be re-initialized
+    // when you hit the "back" button to return to the page.
+    if (Prototype.Browser.Gecko) {
+      window.addEventListener('pageshow', this.onBlur.bind(this), false);
+    }
+  },
+  
+  onFocus: function() {
+    if (this.isGhosted) {
+      this.element.setValue('');
+      this.setGhosted(false);
+    }
+  },
+  
+  onBlur: function() {
+    var value = this.element.getValue();
+    if (value.blank() || value == this.title) {
+      this.setGhosted(true);
+    } else {
+      this.setGhosted(false);
+    }
+  },
+  
+  setGhosted: function(isGhosted) {
+    this.isGhosted = isGhosted;
+    this.element[isGhosted ? 'addClassName' : 'removeClassName']('ghosted');
+    if (isGhosted) {
+      this.element.setValue(this.title);
+    }    
+  },
+
+  // Hook into the enclosing form's `onsubmit` event so that we clear any
+  // ghosted text before the form is sent.
+  onSubmit: function() {
+    if (this.isGhosted) {
+      this.element.setValue('');
+    }
+  }
+});
+
+document.observe("dom:loaded", function() {
+  new Form.GhostedField($('search'), "Search");
 });
