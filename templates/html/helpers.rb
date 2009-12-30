95a589a568d4845d297df2165eeb28229a9e5080
Tobie Langel <tobie.langel@gmail.com>
2009-12-30 04:06:00 +0100

    Add section opening and closing View helpers.

--- templates/html/helpers.rb
+++ templates/html/helpers.rb
@@ -3,6 +3,22 @@ module PDoc
     module Html
       module Helpers
         module BaseHelper
+          def layout_section(title, classname = title.downcase)
+            <<-EOS
+              <div class="section section-#{classname}">
+                <div class="section-title">
+                  <h3>#{title}</h3>
+                </div> <!-- .section-title -->
+                <div class="section-content">
+            EOS
+          end
+          
+          def end_layout_section
+            <<-EOS
+              </div> <!-- .section-content -->
+            </div> <!-- .section -->
+            EOS
+          end
         end
         
         module LinkHelper
