97d71c3ae172eba9bb1376540d88a3a019248b28
Tobie Langel <tobie.langel@gmail.com>
2010-05-11 03:27:25 +0200

    Add JSON generator.

--- lib/pdoc/generators.rb
+++ lib/pdoc/generators.rb
@@ -1,4 +1,5 @@
 require File.expand_path(File.join(File.dirname(__FILE__), 'generators', 'abstract_generator'))
 require File.expand_path(File.join(File.dirname(__FILE__), 'generators', 'html'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'generators', 'json'))
 
 
